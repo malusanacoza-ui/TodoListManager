ae6e3c1cbbe7741e471e7857fc3d88a4632b4a04
malusanacoza-ui <malusana.co.za@gmail.com>
2026-02-06 09:38:42 +0200

    added a Taskcontroller

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TodoListManager.Data;
+
+namespace TodoListManager.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TasksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tasks
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.TodoTasks.Include(t => t.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Tasks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var todoTask = await _context.TodoTasks
+                .Include(t => t.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (todoTask == null)
+            {
+                return NotFound();
+            }
+
+            return View(todoTask);
+        }
+
+        // GET: Tasks/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Tasks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,IsCompleted,IsPinned,Priority,CreatedAt,UserId")] TodoTask todoTask)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(todoTask);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", todoTask.UserId);
+            return View(todoTask);
+        }
+
+        // GET: Tasks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var todoTask = await _context.TodoTasks.FindAsync(id);
+            if (todoTask == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", todoTask.UserId);
+            return View(todoTask);
+        }
+
+        // POST: Tasks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,IsCompleted,IsPinned,Priority,CreatedAt,UserId")] TodoTask todoTask)
+        {
+            if (id != todoTask.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(todoTask);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TodoTaskExists(todoTask.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", todoTask.UserId);
+            return View(todoTask);
+        }
+
+        // GET: Tasks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var todoTask = await _context.TodoTasks
+                .Include(t => t.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (todoTask == null)
+            {
+                return NotFound();
+            }
+
+            return View(todoTask);
+        }
+
+        // POST: Tasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var todoTask = await _context.TodoTasks.FindAsync(id);
+            if (todoTask != null)
+            {
+                _context.TodoTasks.Remove(todoTask);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TodoTaskExists(int id)
+        {
+            return _context.TodoTasks.Any(e => e.Id == id);
+        }
+    }
+}
