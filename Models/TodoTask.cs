a75bd71c59133969c332826a645c8cb44877438a
malusanacoza-ui <malusana.co.za@gmail.com>
2026-02-06 11:16:30 +0200

    updated the create.cshtml to submit the form

--- Models/TodoTask.cs
+++ Models/TodoTask.cs
@@ -1,23 +1,31 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
-public class TodoTask
-{
-    public int Id { get; set; }
 
-    [Required]
-    public string Title { get; set; }
+    public class TodoTask
+    {
+        public int Id { get; set; }
 
-    public string? Description { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
 
-    public bool IsCompleted { get; set; }
+        public string? Description { get; set; }
 
-    public bool IsPinned { get; set; }
+        public bool IsCompleted { get; set; }
 
-    public PriorityLevel Priority { get; set; }
+        public bool IsPinned { get; set; }
 
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [Required]
+        public PriorityLevel Priority { get; set; }
 
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    [ValidateNever]
     public string UserId { get; set; }
-    public IdentityUser User { get; set; }
-}
+
+        [ValidateNever]
+        public IdentityUser User { get; set; }
+    }
+
